9b4511afb0e0bec41264d7da18d645e949c9045d
Hendrik Ziegler <hendrikziegler@outlook.de>
2021-04-12 08:14:48 +0200

    added the javascript for adding and removing elements from the list, now working on the frontent-backend communication

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,22 @@
-console.log("Hello from my Go server")
+const input = document.getElementById("formInput")
+const submit = document.getElementById("submit")
+const foodList = document.getElementById("foodList")
+
+submit.addEventListener("click", () => {
+    let foodName = input.value;
+    const listItem = document.createElement("div")
+    listItem.classList.add("listItem")
+    listItem.innerHTML = `<p>${foodName}</p><button>close</button>`
+    foodList.appendChild(listItem)
+    listItem.querySelector("button").addEventListener("click", (e) => {
+        e.target.parentElement.remove()
+    })
+})
+
+const buttons = foodList.querySelectorAll("button")
+
+buttons.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        e.target.parentElement.remove()
+    })
+})
