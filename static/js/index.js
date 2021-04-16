c28a08229f9633bd1270549b6811f58c229281a5
Hendrik Ziegler <hendrikziegler@outlook.de>
2021-04-16 20:15:43 +0200

    Added expandable Accordions that will contain the Information about the Foods like labels and ingredients

--- static/js/index.js
+++ static/js/index.js
@@ -26,17 +26,50 @@ async function addFood() {
             const listItem = document.createElement("div")
             listItem.classList.add("listItem")
             listItem.innerHTML = `
-            <div class="listItemHeader">
-            <p>${respJson.Name}</p><button>close</button>
+            <button class="listItemHeader">
+                <p>${respJson.Name}</p>
+                <svg
+                    class="arrow"
+                    viewBox="0 0 266 438"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        class="pathColored"
+                        d="m258.476 235.971-194.344 194.343c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901l154.021-154.746-154.021-154.745c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0l194.343 194.343c9.373 9.372 9.373 24.568.001 33.941z"
+                    />
+                </svg>
+            </button>
+            <div class="listItemContent">
+                <div class="labelList"></div>
+                <button class="removeBtn">remove</button>
             </div>
-            <div class="labelList"></div>
             `
             let labelList = listItem.querySelector(".labelList")
             respJson.Label.forEach((e) => {
                 labelList.innerHTML = labelList.innerHTML + `<p>${e}</p>`
             })
             foodList.appendChild(listItem)
-            listItem.querySelector("button").addEventListener("click", async (e) => {
+            listItem.querySelector(".listItemContent").classList.add("collapsed")
+            listItem.querySelector(".listItemHeader").addEventListener("click", () => {
+                let content = listItem.querySelector(".listItemContent")
+                if(content.classList.contains("collapsed")) 
+                {
+                    content.classList.remove("collapsed")
+                }
+                else
+                {
+                    content.classList.add("collapsed")
+                }
+                let svg = listItem.querySelector("svg")
+                if(svg.classList.contains("rotated90"))
+                {
+                    svg.classList.remove("rotated90")
+                }
+                else
+                {
+                    svg.classList.add("rotated90")
+                }
+            })
+            listItem.querySelector(".removeBtn").addEventListener("click", async (e) => {
                 let response = await fetch("/api/editFood", {
                     method: 'DELETE',
                     headers: {
@@ -77,17 +110,50 @@ async function setup() {
         const listItem = document.createElement("div")
             listItem.classList.add("listItem")
             listItem.innerHTML = `
-            <div class="listItemHeader">
-            <p>${el.Name}</p><button>close</button>
+            <button class="listItemHeader">
+                <p>${el.Name}</p>
+                <svg
+                    class="arrow"
+                    viewBox="0 0 266 438"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        class="pathColored"
+                        d="m258.476 235.971-194.344 194.343c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901l154.021-154.746-154.021-154.745c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0l194.343 194.343c9.373 9.372 9.373 24.568.001 33.941z"
+                    />
+                </svg>
+            </button>
+            <div class="listItemContent">
+                <div class="labelList"></div>
+                <button class="removeBtn">remove</button>
             </div>
-            <div class="labelList"></div>
             `
             let labelList = listItem.querySelector(".labelList")
             el.Label.forEach((e) => {
                 labelList.innerHTML = labelList.innerHTML + `<p>${e}</p>`
             })
         foodList.appendChild(listItem)
-        listItem.querySelector("button").addEventListener("click", async (e) => {
+        listItem.querySelector(".listItemContent").classList.add("collapsed")
+        listItem.querySelector(".listItemHeader").addEventListener("click", () => {
+            let content = listItem.querySelector(".listItemContent")
+            if(content.classList.contains("collapsed")) 
+            {
+                content.classList.remove("collapsed")
+            }
+            else
+            {
+                content.classList.add("collapsed")
+            }
+            let svg = listItem.querySelector("svg")
+            if(svg.classList.contains("rotated90"))
+            {
+                svg.classList.remove("rotated90")
+            }
+            else
+            {
+                svg.classList.add("rotated90")
+            }
+        })
+        listItem.querySelector(".removeBtn").addEventListener("click", async (e) => {
             let response = await fetch("/api/editFood", {
                 method: 'DELETE',
                 headers: {
