cab86c84d7fa9ad643c3f3a19e6d1e1a39f60124
Hendrik Ziegler <hendrikziegler@outlook.de>
2021-04-26 11:29:33 +0200

    more setup for the label list, now working on the backend and javascript behind it

--- static/js/foodPlanner.js
+++ static/js/foodPlanner.js
@@ -1,1 +1,43 @@
-console.log("Hello World")
+const generateFoodButton = document.getElementById("generateFood")
+const labelGrid = document.getElementById("labelGrid")
+
+async function generateFood() {
+    let labelConstraints = []
+    document.querySelectorAll('.labelInput').forEach((el) => {
+        if (el.checked) {
+            labelConstraints.push(el.value)
+        }
+    })
+    console.log(JSON.stringify({
+        method: 'Get',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: labelConstraints
+    }))
+}
+
+generateFoodButton.addEventListener("click", async (ev) => {
+    await generateFood()
+})
+
+async function setup() {
+    let label = await fetch("/api/getLabel", {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then( async (r) => {return await r.json()})
+    label.forEach(async (el) => {
+        let newLabelInput = document.createElement('input')
+        newLabelInput.classList.add('labelInput')
+        newLabelInput.type = 'checkbox'
+        newLabelInput.value = el
+        newLabelInput.checked = false
+        let inpLabel = document.createElement('label')
+        inpLabel.appendChild(newLabelInput)
+        inpLabel.innerHTML += el
+        labelGrid.appendChild(inpLabel)
+    })
+}
+setup()
